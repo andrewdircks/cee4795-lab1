0505e0e069f005f961e82fb4ed0b54184f4d5958
Andrew <andrewdircks@me.com>
2022-02-15 20:01:10 -0500

    clean up

--- q3.py
+++ q3.py
@@ -1,7 +1,6 @@
-import numpy as np
 import matplotlib.pyplot as plt
 
-fname = 'data-raw-high-baud.txt'
+fname = 'data-raw-bennett.txt'
 
 def parse_line(line):
     return float(line[6:12]), float(line[16:-1])
@@ -15,24 +14,32 @@ with open(fname, 'r') as f:
         voltages.append(v)
         ts.append(t)
 
-# samples = list(range(len(voltages)))
+samples = list(range(len(voltages)))
 # plt.scatter(samples, voltages)
-# plt.xlabel('Sample #')
-# plt.ylabel('Voltage (V)')
-# plt.show()
-
-n = len(voltages) # number of samples
-dt = ts[-1] - ts[0]
-print(n)
-print(dt)
-
-print(dt / n)
-print(1 / (dt/n))
-
-t0 = [ts[0] + i*(dt / n) for i in range(n)]
-
-plt.plot(ts, voltages)
+plt.plot(samples, voltages)
+plt.xlabel('Sample #')
+plt.ylabel('Voltage (V)')
 plt.show()
 
+# calculate sampling frequency
+n = len(voltages)
+dt = ts[-1] - ts[0]
+print(f'number of samples: {n}')
+print(f'time difference (s): {dt}')
+print(f'average sampling period (s): {dt / n}')
+print(f'average sampling frequency (Hz): {1 / (dt/n)}')
+
+# calculate uniform sampling vector with average sampling period
+t0 = [ts[0] + i*(dt / n) for i in range(n)]
+
+# "raw" time vector plot
+plt.plot(ts, voltages)
+plt.xlabel('Time (s)')
+plt.ylabel('Voltage (V)')
+plt.show()
+
+# interpolated time vector plot
 plt.plot(t0, voltages)
+plt.xlabel('Time (s)')
+plt.ylabel('Voltage (V)')
 plt.show()
